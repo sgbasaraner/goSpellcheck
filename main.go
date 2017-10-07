9d373b35e26ef375c98392146fe2d7a43636e498
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-10-07 15:46:20 +0300

    add lean mode

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "os"
 import "fmt"
 import "bufio"
+import "regexp"
+import "strings"
+
+var lean bool = false
 
 func checkError (e error) {
 	if e != nil {
@@ -10,21 +14,35 @@ func checkError (e error) {
 	}
 }
 
+func isAlpha(str string) bool {
+	for i := range str {
+		if str[i] < 'A' || str[i] > 'z' {
+			return false
+		} else if str[i] > 'Z' && str[i] < 'a' {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 
 	// check command line arguments' count
 	args := os.Args
-	if len(args) != 2 && len(args) != 3 {
-		fmt.Println("Usage: spellcheck [dictionary] text")
+	argLength := len(args)
+
+	if argLength > 4 || argLength < 3 || (argLength == 4 && args[3] != "lean") {
+		fmt.Println("Usage: spellcheck text dictionary [lean]")
+		os.Exit(2)
 	}
 
-	// decide which dictionary to load
-	var dictionary string
-
-	if len(args) != 3 {
-		dictionary = "dictionaries/large"
-	} else {
-		dictionary = string(args[1])
+	// evaluate args
+	// decide which dictionary & text will be loaded
+	// set to lean mode (optional)
+	var dictionary string = args[2]
+	var textFile string = args[1]
+	if argLength == 4{
+		lean = true
 	}
 
 	// load the dictionary
@@ -32,15 +50,6 @@ func main() {
 	if !loaded {
 		fmt.Println("Unable to load", dictionary)
 		os.Exit(1)
-	}
-
-	// decide which text file to spellcheck
-	var textFile string
-
-	if len(args) != 3 {
-		textFile = string(args[1])
-	} else {
-		textFile = string(args[2])
 	}
 
 	fmt.Println("\nMISSPELLED WORDS\n")
@@ -52,14 +61,43 @@ func main() {
 	scanner := bufio.NewScanner(text)
 	scanner.Split(bufio.ScanWords)
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		words += 1
-		if !check(line){
-			fmt.Println(line)
-			misspellings += 1
+	if lean {
+		for scanner.Scan() {
+			line := scanner.Text()
+			if !check(line){
+				fmt.Println(line)
+				misspellings += 1
+			}
+			words += 1
+		}
+	} else {
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			// ignore words with numbers like MS Word
+			match, err := regexp.MatchString("[0-9]+", line)
+			checkError(err)
+
+			// reconstruct the word to take only alphabetic characters and apostrophe
+			var word string
+			for i := 0; i < len(line); i++ {
+				if isAlpha(string(line[i])) || (line[i] == '\'') {
+					word = word + string(line[i])
+				}
+			}
+
+			word = strings.TrimSpace(word)
+			// ignore too long words
+			if !match && len(word) <= 45{
+				if !check(word) && word != ""{
+					fmt.Println(word)
+					misspellings += 1
+				}
+			}
+			words += 1
 		}
 	}
+	
 
 	fmt.Println("\nWORDS MISSPELLED:", misspellings)
 	fmt.Println("WORDS IN DICTONARY:", size())
