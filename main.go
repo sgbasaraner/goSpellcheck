1ebb9ca10cd57b504761af23a7e6b0bf5f684faa
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-10-07 08:05:06 +0300

    add ability to read from file

--- main.go
+++ main.go
@@ -2,9 +2,23 @@ package main
 
 import "os"
 import "fmt"
+import "io/ioutil"
+
+func check (e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
-	arg := os.Args[1]
 
-	fmt.Println(arg)
+	// check command line arguments' count
+	args := os.Args
+	if len(args) != 2 && len(args) != 3 {
+		fmt.Println("Usage: spellcheck [dictionary] text")
+	}
+
+	dat, err := ioutil.ReadFile(args[1])
+	check(err)
+	fmt.Print(string(dat))
 }
