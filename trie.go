3a8ed8f7302c5ff151b221b59ac27a6fe51d514e
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-10-07 17:16:37 +0300

    remove debugging code

--- trie.go
+++ trie.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type trieNode struct {
 	label 		byte
 	children 	[]*trieNode
@@ -12,12 +10,6 @@ func (n *trieNode) AddChild(l byte) *trieNode {
 	np := &node
 	n.children = append(n.children, np)
 	return np
-}
-
-func (n *trieNode) PrintChildren() {
-	for i := 0; i < len(n.children); i++ {
-		fmt.Println(string(n.children[i].label))
-	}
 }
 
 func (n *trieNode) HasChild(l byte) *trieNode {
@@ -33,20 +25,18 @@ func (n *trieNode) AddWord(word string) {
 	cursor := n
 	for i := 0; i < len(word); i++ {
 		if cursor.HasChild(byte(word[i])) == nil {
-			// fmt.Println("cursor is:", cursor.label)
 			cursor = cursor.AddChild(byte(word[i]))
 		} else {
 			cursor = cursor.HasChild(byte(word[i]))
 		}
 	}
-	// tilde indicates end of the word
+	// tilde indicates the end of the word
 	cursor.AddChild(byte('~'))
 }
 
 func (n *trieNode) Search(word string) bool {
 	cursor := n
 	for i := 0; i < len(word); i++ {
-		// cursor.PrintChildren()
 		if cursor.HasChild(byte(word[i])) != nil {
 			cursor = cursor.HasChild(byte(word[i]))
 		} else {
